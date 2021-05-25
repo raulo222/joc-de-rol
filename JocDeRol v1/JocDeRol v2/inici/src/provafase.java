746c853b75cdc894ab2f1b9f90a81e0320f5b2cc
raulo222 <53102081+raulo222@users.noreply.github.com>
2021-05-25 09:46:54 +0200

    Add files via upload

---
+++ JocDeRol v1/JocDeRol v2/inici/src/provafase.java
@@ -0,0 +1,7 @@
+public class provafase {
+    public static void main(String[] args) {
+        Human huma1 = new Human("Raül",16,2,900);
+        Alien alien1 = new Alien("ET",22,2,10);
+        alien1.attack(huma1);
+    }
+}
